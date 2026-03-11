f6be6f4ac42f6f8208f6c6b8f05954c230ed3190
Malcolm Skoglund <DIN_EMAIL_HR>
2026-03-11 19:17:02 +0100

    add backup policy and terraform ci

--- main.tf
+++ main.tf
@@ -38,3 +38,27 @@ resource "google_compute_instance" "vm" {
 
   tags = ["lab1", "ssh"]
 }
+resource "google_compute_resource_policy" "daily_backup" {
+  name   = "${var.student_id}-daily-backup"
+  region = var.region
+
+  snapshot_schedule_policy {
+    schedule {
+      daily_schedule {
+        days_in_cycle = 1
+        start_time    = "03:00"
+      }
+    }
+
+    retention_policy {
+      max_retention_days    = 7
+      on_source_disk_delete = "KEEP_AUTO_SNAPSHOTS"
+    }
+  }
+}
+
+resource "google_compute_disk_resource_policy_attachment" "backup_attachment" {
+  name = google_compute_resource_policy.daily_backup.name
+  disk = google_compute_instance.vm.name
+  zone = "${var.region}-a"
+}
